9d6b2f6b3274ac3c4102e9384b3624464284c4d9
eduard.dereza <eduard.dereza@pearlgroup.no>
2023-07-24 19:08:38 +0300

    add tailscale to homer config

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #docker login --username=deduard
-docker build -t deduard/homelab:homer . --rm
-docker push deduard/homelab:homer
+docker build -t deduard/homelab:homer-$(date +%Y%m%d) . --rm
+docker push deduard/homelab:homer-$(date +%Y%m%d)
